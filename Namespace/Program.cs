df6289e76d1d4462dc534b5f07ee0f7ec9c9a716
hasher116 <andrei13081996@mail.ru>
2021-07-26 13:22:51 +0300

    Добавьте файлы проекта.

---
+++ Namespace/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Namespace.Andrew;
+using Vasya.hiVasya;
+
+namespace Namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            NameAndrew Indrew = new NameAndrew();
+            NameIgor Igor = new NameIgor();
+            NameVasya vasya2 = new NameVasya();
+            Vasya.hiVasya.NameVasya vasya = new Vasya.hiVasya.NameVasya();
+        }
+    }
+
+    class NameIgor
+    {
+        static NameIgor()
+        {
+            Console.WriteLine("\nhi, Igor");
+        }
+    }
+
+    namespace Andrew
+    {
+        class NameAndrew
+        {
+            static NameAndrew()
+            {
+                Console.WriteLine("\nhi, Andrew");
+            }
+        }
+    }
+}
+
+namespace Vasya
+{
+    namespace hiVasya
+    {
+
+        class NameVasya
+        {
+            static NameVasya()
+            {
+                Console.WriteLine("\nhi, Vasya"); ;
+            }
+        }
+    }
+}
